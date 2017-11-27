d3c9ac66ac6fc83bf3c48f98e8f91a231218dcfb
Netanel Fried <netanel.fried@ibm.com>
2017-11-27 19:36:16 +0200

    bla bla or goren

--- main.c
+++ main.c
@@ -28,6 +28,13 @@ int main(int argc, char** argv)
 
 	//printList(l);
 
-	int i = convertHexToIntTwosCom("FFFFAB123");
-	printf("%d\n", i);
+	int k = charToInt('A');
+	int j = charToInt('F');
+	int l = charToInt('0');
+	int m = charToInt('9');
+//	int i = convertHexToIntTwosCom("FFFFAB123");
+	printf("A: %d\n", k);
+	printf("F: %d\n", j);
+	printf("0: %d\n", l);
+	printf("9: %d\n", m);
 }
